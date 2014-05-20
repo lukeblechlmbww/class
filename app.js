eb7244f98480981def2e7378035bdebfdeba2109
Blechl, Luke <luke.blechl@mbww.com>
2014-05-20 11:29:02 -0700

    Added me route

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('<H1>Hello</H1> Express');
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
+app.get('/me', function(req, res) {
+  res.send('@planetoftheweb');
 });
