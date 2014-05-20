35121ae213b3b2509aef490d612856b937a952a3
Blechl, Luke <luke.blechl@mbww.com>
2014-05-20 11:31:44 -0700

    Added luke route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/me', function(req, res) {
   res.send('@planetoftheweb');
 });
 
+app.get('/luke', function(req, res) {
+  res.send('Yo Yo Brahj!');
+});
+
 var server = app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
